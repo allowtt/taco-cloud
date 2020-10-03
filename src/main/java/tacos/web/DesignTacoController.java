85a8bd2788e990819edd7ccb894e58c5ec3788bf
lgy <allowttt@naver.com>
2020-10-03 14:20:41 +0900

    10/02

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,59 +1,101 @@
 package tacos.web;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.Errors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import tacos.Order;
+import tacos.data.IngredientRepository;
+
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
-import tacos.Taco;
+import tacos.data.TacoRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
+	private final IngredientRepository ingredientRepo;
+	
+	private TacoRepository tacoRepo;
+
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DesignTacoController.class);
+
+	@Autowired
+	public DesignTacoController(
+			IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+	  this.ingredientRepo = ingredientRepo;
+	  this.tacoRepo = tacoRepo;
+	}
+
 	@GetMapping
-	public String showDesignForm(Model model) {
-		
-		List<Ingredient> ingredients = Arrays.asList(
-			      new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-			      new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-			      new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-			      new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-			      new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-			      new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-			      new Ingredient("CHED", "Cheddar", Type.CHEESE),
-			      new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-			      new Ingredient("SLSA", "Salsa", Type.SAUCE),
-			      new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
-		
-		Type[] types = Ingredient.Type.values();
-		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-		}
-		model.addAttribute("taco", new Taco());
-		return "design";
-	}
+	  public String showDesignForm(Model model) {
+	    
+		List<Ingredient> ingredients = new ArrayList<>();
+	    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
+	    Type[] types = Ingredient.Type.values();
+	    log.info("types : "+types.toString());
+	    for (Type type : types) {
+	      model.addAttribute(type.toString().toLowerCase(),
+	          filterByType(ingredients, type));
+	    }
+
+	    model.addAttribute("taco", new Taco());
+	    log.info("model.toString() : "+model.toString());
+	    return "design";
+	  }
 	
-	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
-	}
-	
-	@PostMapping
-	public String processDesign(Taco design) {
-		
-		//이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다.
-		//이 작업은 3장에서 할 예정(TODO)
-		log.info("Processing design: "+design);
-		return "redirect:/orders/current";
-	}
+	  private List<Ingredient> filterByType(
+	      List<Ingredient> ingredients, Type type) {
+	    return ingredients
+	              .stream()
+	              .filter(x -> x.getType().equals(type))
+	              .collect(Collectors.toList());
+	  }
+
+	  @ModelAttribute(name = "order")
+	  public Order order() {
+	    return new Order();
+	  }
+
+	  @ModelAttribute(name = "taco")
+	  public Taco taco() {
+	    return new Taco();
+	  }
+
+	  @PostMapping
+	  public String processDesign(
+			  @Valid Taco design, 
+			  Errors errors, @ModelAttribute Order order) {
+		  if (errors.hasErrors()) {
+			 return "design";
+		  }
+		  log.info("processDesign1");
+		  log.info("design : "+design);
+		  Taco saved = tacoRepo.save(design);
+		  log.info("processDesign2");
+		  order.addDesign(saved);
+		  log.info("processDesign3");
+
+		  return "redirect:/orders/current";
+	  }
+
 }
